aeeb33fa598c2d629fc3935fd4f34cb86fb88fd0
Sameer <sameer.q08@gmail.com>
2023-09-27 16:51:08 +0530

    "ListViewBuilder"

--- lib/View/home.dart
+++ lib/View/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../Model/coinmodel.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,6 +11,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    getCoinMarket();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     double myHeight = MediaQuery.of(context).size.height;
@@ -39,16 +48,16 @@ class _HomeState extends State<Home> {
                     decoration: BoxDecoration(
                         color: Colors.white.withOpacity(0.5),
                         borderRadius: BorderRadius.circular(5)),
-                    child: Text(
+                    child: const Text(
                       'Main Portfolio',
                       style: TextStyle(fontSize: 18),
                     ),
                   ),
-                  Text(
+                  const Text(
                     'Top 10 coins',
                     style: TextStyle(fontSize: 18),
                   ),
-                  Text(
+                  const Text(
                     'Experimental',
                     style: TextStyle(fontSize: 18),
                   ),
@@ -56,20 +65,41 @@ class _HomeState extends State<Home> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: EdgeInsets.symmetric(horizontal: myWidth * 0.07),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('Main '),
+                  const Text(
+                    '\$ 7,466.20',
+                    style: TextStyle(fontSize: 30),
+                  ),
                   Container(
-                    height: myHeight * 0.04,
+                    padding: EdgeInsets.all(myWidth * 0.02),
+                    height: myHeight * 0.05,
                     width: myWidth * 0.1,
                     decoration: BoxDecoration(
-                      shape: BoxShape.circle,
+                        shape: BoxShape.circle,
+                        color: Colors.white.withOpacity(0.5)),
+                    child: Image.asset(
+                      'assets/icons/5.1.png',
                     ),
-                  )
+                  ),
                 ],
               ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: myWidth * 0.07),
+              child: Row(
+                children: [
+                  Text(
+                    '+ 162% all time',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: myHeight * 0.02,
             ),
             Container(
               height: myHeight * 0.7,
@@ -80,10 +110,70 @@ class _HomeState extends State<Home> {
                     topLeft: Radius.circular(50),
                     topRight: Radius.circular(50),
                   )),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: myHeight * 0.02,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: myWidth * 0.08),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Assets',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Icon(Icons.add)
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: isRefreshing == true
+                        ? Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : ListView.builder(
+                            itemCount: coinMarket!.length,
+                            itemBuilder: (context, indext) {
+                              return const Text('data');
+                            }),
+                  )
+                ],
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  bool isRefreshing = true;
+
+  List? coinMarket = [];
+  var coinMarketList;
+  Future<List<CoinModel>?> getCoinMarket() async {
+    const url =
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&sparkline=true';
+
+    setState(() {
+      isRefreshing = true;
+    });
+    var response = await http.get(Uri.parse(url), headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    });
+    setState(() {
+      isRefreshing = false;
+    });
+    if (response.statusCode == 200) {
+      var x = response.body;
+      coinMarketList = coinModelFromJson(x);
+      setState(() {
+        coinMarket = coinMarketList;
+      });
+    } else {
+      print(response.statusCode);
+    }
   }
 }
