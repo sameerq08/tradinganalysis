c80753d2e0346d182ac9c5fee1522bdd3448297f
Sameer <sameer.q08@gmail.com>
2023-10-04 10:47:19 +0530

    "ScreenDesign"

--- lib/View/Components/item.dart
+++ lib/View/Components/item.dart
@@ -15,68 +15,93 @@ class Item extends StatelessWidget {
       child: Container(
         child: Row(
           children: [
-            Container(
-              height: myHeight * 0.05,
-              child: Image.network(item.image),
-            ),
-            SizedBox(
-              width: myWidth * 0.03,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  item.id,
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  '0.4' + item.symbol,
-                  style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey),
-                )
-              ],
-            ),
-            SizedBox(
-              width: myWidth * 0.03,
-            ),
-            Container(
-              height: myHeight * 0.05,
-              width: myWidth * 0.1,
-              child: Sparkline(
-                data: item.sparklineIn7D.price,
-                lineWidth: 2.0,
-                lineColor: Colors.red,
-                fillMode: FillMode.below,
-                fillGradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: const [0.0, 0.7],
-                    colors: [Colors.red, Colors.red.shade100]),
+            Expanded(
+              flex: 2,
+              child: Container(
+                height: myHeight * 0.05,
+                child: Image.network(item.image),
               ),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '\$ ' + item.currentPrice.toString(),
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
+            SizedBox(
+              width: myWidth * 0.03,
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.id,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '0.4' + item.symbol,
+                    style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              width: myWidth * 0.03,
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                height: myHeight * 0.05,
+                width: myWidth * 0.1,
+                child: Sparkline(
+                  data: item.sparklineIn7D.price,
+                  lineWidth: 2.0,
+                  lineColor: Colors.red,
+                  fillMode: FillMode.below,
+                  fillGradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: const [0.0, 0.7],
+                      colors: [Colors.red, Colors.red.shade100]),
                 ),
-                Row(
-                  children: [
-                    Text(
-                      item.priceChange24H.toString(),
-                      style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey),
-                    ),
-                  ],
-                )
-              ],
+              ),
+            ),
+            SizedBox(
+              width: myWidth * 0.03,
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '\$ ${item.currentPrice}',
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        item.priceChange24H.toString(),
+                        style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                      ),
+                      SizedBox(
+                        width: myWidth * 0.03,
+                      ),
+                      Text(
+                        item.marketCapChangePercentage24H.toStringAsFixed(2),
+                        style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ],
         ),
